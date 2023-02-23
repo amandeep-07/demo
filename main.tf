dc2526f356f046ea75e3866275a4db1e43fca654
amandeep-07 <125429714+amandeep-07@users.noreply.github.com>
2023-02-23 13:51:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "ec2_instance" {
   ami                       = "ami-0f8ca728008ff5af4"
   instance_type             = "t2.medium"
   key_name                  = "Amandeep"
-  user_data = <<EOF
+  user_data = <<-EOF
         #! /bin/bash
         sudo apt-get update
         sudo apt install unzip
