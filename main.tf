77c5e522aa1a1bc44ea77bcfa29f7fca6d7b6fe5
amandeep-07 <125429714+amandeep-07@users.noreply.github.com>
2023-02-23 13:08:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,17 +17,17 @@ resource "aws_instance" "ec2_instance" {
   ami                       = "ami-0f8ca728008ff5af4"
   instance_type             = "t2.medium"
   key_name                  = "Amandeep"
-  user_data = << EOF
-    #! /bin/bash
-    sudo apt-get update
-    sudo apt install unzip
-    sudo apt-add-repository ppa:ansible/ansible
-    sudo apt update
-    sudo apt install ansible -y
-    sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    sudo unzip awscliv2.zip
-    sudo ./aws/install
-   EOF
+  user_data = <<EOF
+        #! /bin/bash
+        sudo apt-get update
+        sudo apt install unzip
+        sudo apt-add-repository ppa:ansible/ansible
+        sudo apt update
+        sudo apt install ansible -y
+        sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        sudo unzip awscliv2.zip
+        sudo ./aws/install
+  EOF
   tags                      = {
     Name = "Testing"
   }
