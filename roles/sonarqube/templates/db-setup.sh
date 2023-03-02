6432d84fefe68a7429ecb775e75c2fb175e42923
amandeep-07 <125429714+amandeep-07@users.noreply.github.com>
2023-03-02 14:44:01 +0530

    Delete roles/sonarqube directory

--- roles/sonarqube/templates/db-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-CREATE USER sonarqube ;
-echo "CREATE USER sonarqube WITH PASSWORD 'Password'; CREATE DATABASE sonarqube OWNER sonarqube; GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonarqube; \q" | sudo -u postgres psql;
